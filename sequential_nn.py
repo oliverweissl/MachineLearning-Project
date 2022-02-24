3e0bc5b6d06c681a99f4f7bf13d4247aa5942312
Oliver <o.weissl@student.vu.nl>
2022-02-24 12:35:20 +0100

    updated

--- sequential_nn.py
+++ sequential_nn.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 def create_model(input_shape):
-    IMG_SIZE = input_shape[0]
+
     inputs = keras.Input(shape=input_shape)
     data_augmentation = keras.Sequential(
         [
@@ -12,10 +12,10 @@ def create_model(input_shape):
     )
     x = data_augmentation(inputs)
     x = layers.Flatten()(x)
-    x = layers.Dense(IMG_SIZE*6, activation="relu")(x)
-    x = layers.Dense(IMG_SIZE*4, activation="relu")(x)
-    x = layers.Dense(IMG_SIZE*2, activation="relu")(x)
-    x = layers.Dense(IMG_SIZE, activation="relu")(x)
+    x = layers.Dense(512, activation="relu")(x)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dense(64, activation="relu")(x)
 
     outputs = layers.Dense(units=26, activation="softmax")(x)
     return keras.Model(inputs, outputs)
