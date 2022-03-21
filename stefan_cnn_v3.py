7d4ea7acfaf8632daf64fcc91a74ce5a8a8b1ab0
Oliver <o.weissl@student.vu.nl>
2022-03-21 20:32:00 +0100

    final_testing

--- stefan_cnn_v3.py
+++ stefan_cnn_v3.py
@@ -3,61 +3,48 @@ from tensorflow.keras import layers
 from tensorflow.keras.regularizers import l2
 
 def create_model(input_shape, num_classes):
-    # Note: input is flipped to (height, width) instead of (width, height)
     inputs = keras.Input(shape=(input_shape[1], input_shape[0], input_shape[2]))
 
-    data_augmentation = keras.Sequential()
+    data_augmentation = keras.Sequential(
+        [
+            layers.experimental.preprocessing.RandomRotation(0.1),
+        ]
+    )
     x = data_augmentation(inputs)
 
-    ACTIVATION_STR = "swish"
-
-
-    # Entry block
-    #version tf 2.4.1
-    #x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
-    #-----------------
-    x = layers.Conv2D(16, 3, strides=2, padding="same",activation =ACTIVATION_STR,  kernel_regularizer = l2(1e-4))(x)
+    x = layers.Conv2D(16, 3, strides=2, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(32, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
+    x = layers.Conv2D(32, 3, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(32, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
+    x = layers.Conv2D(32, 3, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
     x = layers.BatchNormalization()(x)
-
     x = layers.Dropout(0.1)(x)
 
-    previous_block_activation = x  # Set aside residual
+    previous_block_activation = x
 
-    for size in [64,128]:
-        x = layers.SeparableConv2D(size, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
+    for size in [64]:
+        x = layers.SeparableConv2D(size, 3, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
         x = layers.BatchNormalization()(x)
 
-
-        x = layers.SeparableConv2D(size, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.SeparableConv2D(size, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
+        x = layers.SeparableConv2D(size, 3, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
         x = layers.BatchNormalization()(x)
 
         x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
         # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same", kernel_regularizer = l2(1e-4))(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        x = layers.Dropout(0.1)(x)
-        previous_block_activation = x  # Set aside next residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same", kernel_regularizer = l2(1e-4))(previous_block_activation)
+        x = layers.add([x, residual])
 
-    x = layers.SeparableConv2D(256, 3, padding="same",activation =ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
+        x = layers.Dropout(0.1)(x)
+        previous_block_activation = x
+
+    x = layers.SeparableConv2D(128, 3, padding="same",activation = "swish", kernel_regularizer = l2(1e-4))(x)
     x = layers.BatchNormalization()(x)
+
     x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-    # x = layers.GlobalAveragePooling2D()(x)
     x = layers.Flatten()(x)
 
-    #x = layers.Dense(FC_LAYER, activation=ACTIVATION_STR, kernel_regularizer = l2(1e-4))(x)
-
-    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(num_classes, activation="softmax")(x)
     return keras.Model(inputs, outputs)
